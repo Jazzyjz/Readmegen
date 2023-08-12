64636a22ade665bf25499d9702199b8f01e4205c
Jazzyjz <jasminf2202@gmail.com>
2023-08-12 11:27:45 -0400

    made new js files

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,24 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require ('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const generateMarkdown = require("./utils/generateMarkdown");
+const questions = require("./utils/questions");
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile('README.md', data) {};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    try{const answers =  inquirer.prompt(questions);
+    // answers.licenseBadge = licenseBadge(answers.license);
+    let readMeData = generateMarkdown(answers)
+ fs.writeFile("created-README.md", readMeData);
+} catch(err){
+    throw err;
+}
+}
 
 // Function call to initialize app
 init();
